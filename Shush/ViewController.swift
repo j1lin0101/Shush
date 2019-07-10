3b04fc65e26a952880ff64ef7e55381d35fc12a2
Jeremy <j1lin0101@gmail.com>
2019-07-10 10:08:10 -0700

    Initial Commit

---
+++ Shush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shush
+//
+//  Created by Jeremy Lin on 7/10/19.
+//  Copyright © 2019 Jeremy Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
