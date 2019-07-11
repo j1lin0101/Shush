e40c5fc69ba89ee67bb4baba081e1f0ae5711767
Jeremy <j1lin0101@gmail.com>
2019-07-10 19:27:26 -0700

    AudioKit and Decibel Reading

--- Shush/ViewController.swift
+++ Shush/ViewController.swift
@@ -1,20 +1,69 @@
-//
-//  ViewController.swift
-//  Shush
-//
-//  Created by Jeremy Lin on 7/10/19.
-//  Copyright © 2019 Jeremy Lin. All rights reserved.
-//
-
+import AudioKit
+import AudioKitUI
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private var amplitudeLabel: UILabel!
+    @IBOutlet private var audioInputPlot: EZAudioPlot!
+    
+    var mic: AKMicrophone!
+    var tracker: AKFrequencyTracker!
+    var silence: AKBooster!
+    
+    
+    func setupPlot() {
+        let plot = AKNodeOutputPlot(mic, frame: audioInputPlot.bounds)
+        plot.translatesAutoresizingMaskIntoConstraints = false
+        plot.plotType = .rolling
+        plot.shouldFill = true
+        plot.shouldMirror = true
+        plot.color = UIColor.blue
+        audioInputPlot.addSubview(plot)
+        
+        // Pin the AKNodeOutputPlot to the audioInputPlot
+        var constraints = [plot.leadingAnchor.constraint(equalTo: audioInputPlot.leadingAnchor)]
+        constraints.append(plot.trailingAnchor.constraint(equalTo: audioInputPlot.trailingAnchor))
+        constraints.append(plot.topAnchor.constraint(equalTo: audioInputPlot.topAnchor))
+        constraints.append(plot.bottomAnchor.constraint(equalTo: audioInputPlot.bottomAnchor))
+        constraints.forEach { $0.isActive = true }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        AKSettings.audioInputEnabled = true
+        mic = AKMicrophone()
+        tracker = AKFrequencyTracker(mic)
+        silence = AKBooster(tracker, gain: 0)
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AudioKit.output = silence
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+        setupPlot()
+        Timer.scheduledTimer(timeInterval: 0.4,
+                             target: self,
+                             selector: #selector(ViewController.updateUI),
+                             userInfo: nil,
+                             repeats: true)
+        
+    }
+    
+    @objc func updateUI() {
+        let db = (10 * log(tracker.amplitude)) + 98
+        
+        amplitudeLabel.text = String(format: "Amplitude: %0.1f", db)
+        if (db > 90) {
+            print("You Loud")
+        }
+    }
+    
 }
 
